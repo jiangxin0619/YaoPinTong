76d0a1d85feed87749fe913bf460357397ef2045
jiangxin619 <jiangxin0619@163.com>
2015-04-30 09:55:49 +0800

    组长第一次修改

--- app/src/main/java/com/health/yaopintong/MainActivity.java
+++ app/src/main/java/com/health/yaopintong/MainActivity.java
@@ -16,16 +16,14 @@ import android.widget.RadioGroup;
 
 import com.slidingmenu.lib.SlidingMenu;
 
-/**
- * 主界面
- */
-public class MainActivity extends FragmentActivity {
-private RadioGroup rg;
-    private ListView search_lv;
-    private ImageView left,right;
 
-    private int[] rbIds={R.id.search_btn1,R.id.search_btn2,R.id.search_btn3};
-    private View[] views=new View[3];
+public class MainActivity extends FragmentActivity {
+    private RadioGroup rg;
+    private ListView search_lv;
+    private ImageView left, right;
+
+    private int[] rbIds = {R.id.search_btn1, R.id.search_btn2, R.id.search_btn3};
+    private View[] views = new View[3];
 
     public MainActivity() {
     }
@@ -34,13 +32,13 @@ private RadioGroup rg;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final SlidingMenu menu = new SlidingMenu(this);
-        search_lv= ((ListView) findViewById(R.id.search_lv));
+//        final SlidingMenu menu = new SlidingMenu(this);
+        search_lv = ((ListView) findViewById(R.id.search_lv));
 
-        left= ((ImageView) findViewById(R.id.top_left_img));
+        left = ((ImageView) findViewById(R.id.top_left_img));
 
         //头部右按钮
-        right= ((ImageView) findViewById(R.id.top_right_img));
+        right = ((ImageView) findViewById(R.id.top_right_img));
         right.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,14 +46,14 @@ private RadioGroup rg;
             }
         });
         //对症下药那些RadioButton
-        rg= ((RadioGroup) findViewById(R.id.search_rg));
+        rg = ((RadioGroup) findViewById(R.id.search_rg));
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                for(int i=0;i<rbIds.length;i++){
-                    if(rbIds[i]==checkedId){
+                for (int i = 0; i < rbIds.length; i++) {
+                    if (rbIds[i] == checkedId) {
                         views[i].setVisibility(View.VISIBLE);
-                    }else{
+                    } else {
                         views[i].setVisibility(View.INVISIBLE);
                     }
                 }
@@ -64,10 +62,11 @@ private RadioGroup rg;
         });
         init();
     }
+
     private void init() {
-        views[0]=findViewById(R.id.search_view1);
-        views[1]=findViewById(R.id.search_view2);
-        views[2]=findViewById(R.id.search_view3);
+        views[0] = findViewById(R.id.search_view1);
+        views[1] = findViewById(R.id.search_view2);
+        views[2] = findViewById(R.id.search_view3);
     }
 
 
